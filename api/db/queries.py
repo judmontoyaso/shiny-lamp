83924fba86919e635ac81d9275671f752d350099
Juanda <juandavidsolorzano73@gmail.com>
2024-01-15 18:46:57 -0500

    crear grafica de alpha shannon en js consumiendo los datos de un exp filtrado desde la api de fastapi

--- api/db/queries.py
+++ api/db/queries.py
@@ -1,7 +1,6 @@
 import pandas as pd
 from .session_manager import session_scope
-from ..models.base import Microbiome
-from ..models.base import Taxonomy
+from ..models.base import Microbiome, Taxonomy, FeatureCountExtendedView
 
 # Consultas
 def get_taxonomy_all(limit=100):
@@ -28,6 +27,32 @@ def get_microbiome_by_runid(run_id=None, limit=100):
         # Filtrar por runId si se proporciona
         if run_id:
             query = query.filter(Microbiome.runId == run_id)
+
+        # Aplicar el límite
+        query = query.limit(limit)
+
+        # Ejecutar la consulta y convertir el resultado en un diccionario
+        df = pd.read_sql(query.statement, session.bind)
+        return df.to_dict(orient='records')
+    
+    
+def get_featureCountExtendedView_all(limit=100):
+    """Consulta a la tabla FeatureCountExtendedView con un límite especificado y opcionalmente filtra por runId."""
+    with session_scope() as session:
+        query = session.query(FeatureCountExtendedView).limit(limit)
+        df = pd.read_sql(query.statement, session.bind)
+        return df.to_dict(orient='records')
+    
+    
+def get_featureCountExtendedView_by_project_id(project_id=None, limit=100):
+    """Consulta a la tabla Microbiome con un límite especificado y opcionalmente filtra por runId."""
+    with session_scope() as session:
+        # Iniciar la consulta
+        query = session.query(FeatureCountExtendedView)
+
+        # Filtrar por runId si se proporciona
+        if project_id:
+            query = query.filter(FeatureCountExtendedView.projectId == project_id)
 
         # Aplicar el límite
         query = query.limit(limit)
--- api/routes/Router.py
+++ api/routes/Router.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, FastAPI
 from fastapi.responses import JSONResponse
-from ..db.queries import get_microbiome_all, get_taxonomy_all, get_microbiome_by_runid
+from ..db.queries import get_microbiome_all, get_taxonomy_all, get_microbiome_by_runid, get_featureCountExtendedView_all, get_featureCountExtendedView_by_project_id
 
 router = APIRouter() 
 
@@ -19,5 +19,12 @@ async def read_microbiome(run_id: str, limit: int = 100):
         data = get_microbiome_by_runid(run_id, limit)
         return data
  
- 
- 
+@router.get("/api/featureCount")
+async def get_featurecount_data():
+    data = get_featureCountExtendedView_all()
+    return JSONResponse(content={"data": data})
+
+@router.get("/api/featureCount/{project_id}")
+async def read_microbiome(project_id: str, limit: int = 100):
+        data = get_featureCountExtendedView_by_project_id(project_id, limit)
+        return data
